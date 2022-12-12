6cd94cf87df910543b9e98d923bf2764c9b529fb
Nguyễn Tấn Cường <57739244+tancuong17@users.noreply.github.com>
2022-12-12 15:05:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const Nexmo = require('nexmo');
 const nexmo = new Nexmo({
-	apiKey: "e88c88e0",
-  	apiSecret: "QleCs7DioqVO0R6k"
+	apiKey: "",
+  	apiSecret: ""
 });
 const io = require('socket.io')(http, {
   cors: {
